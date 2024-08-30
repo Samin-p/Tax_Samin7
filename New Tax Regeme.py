af81f81647b06887351e45b82c4836b3d6ca9189
Samin-p <hasan.abul3378@gmail.com>
2024-08-30 22:11:44 +0530

    Update New Tax Regeme.py

--- New Tax Regeme.py
+++ New Tax Regeme.py
@@ -1,12 +1,17 @@
 import pyttsx3 as r
 from num2words import num2words
-import os
+import os 
+import threading
+import time
 y=r.init()
 y.setProperty("rate",150)
 def speak(set):
     y.say(set)
     y.runAndWait()
-speak("This is a progarm to calculate new tax regeme")
+speak(" This is a progarm to calculate new tax regeme")
+def c(f):
+    o=threading.Thread(target=f)
+    o.start()
 class tax:
     def __init__(self,x):
         self.gross=x
@@ -14,40 +19,41 @@ class tax:
         def cess(x):
             return 4/100*x 
         if net_nettaxable<=300000:
-            speak("Nil.you don't have to pay tax")
-            print("Nil.you don't have to pay tax")
+            c(speak("Nil.you don't have to pay tax"))
+            c(print("Nil.you don't have to pay tax"))
         elif 300000<net_nettaxable<=700000:
-                print("No tax charge under section 87 A rebate ")
-                speak("No tax charge under section 87 A rebate ")
+                c(print(f"No tax charge under section 87 A rebate "))
+                c(speak("No tax charge under section 87 A rebate "))
         elif 700000<net_nettaxable<=1000000:
             hero=400000*5/100
             hero1=(net_nettaxable-700000)*10/100
-            print(cess(hero1+hero)+hero+hero1)
+            c(print(cess(hero1+hero)+hero+hero1))
             op=cess(hero1+hero)+hero+hero1
-            speak(f"You have to pay{num2words(op)} ")
+            c(speak(f"You have to pay{num2words(op)} "))
         elif 1000000<net_nettaxable<=1200000:
             hero=400000*5/100
             new=300000*10/100
             hero2=(net_nettaxable-1000000)*15/100
-            print(cess(hero+new+hero2)+hero+new+hero2)
-            speak(f"You have to give{num2words(cess(hero+new+hero2)+hero+new+hero2)}")
+            c(print(cess(hero+new+hero2)+hero+new+hero2))
+            c(speak(f"You have to give{num2words(cess(hero+new+hero2)+hero+new+hero2)}"))
         elif 1200000<net_nettaxable<=1500000:
             hero=400000*5/100
             new=300000*10/100
             hero4=200000*15/100
             hero5=(net_nettaxable-1200000)*20/100
-            print(cess(hero+new+hero4+hero5)+hero+new+hero4+hero5)
-            #speak(f"You have to give{cess(hero+new+hero4+hero5)+hero+new+hero4+hero5}")
+            c(print(cess(hero+new+hero4+hero5)+hero+new+hero4+hero5))
+            c(speak(f"You have to give{cess(hero+new+hero4+hero5)+hero+new+hero4+hero5}"))
         else :
             hero=400000*5/100 
             new=300000*10/100
             hero4=200000*15/100
             hero5=300000*20/100
             hero6=(net_nettaxable-1500000)*30/100
-            print(cess(hero+new+hero4+hero5+hero6)+hero+new+hero4+hero5+hero6)
-            speak(f"You have to give{cess(hero+new+hero4+hero5+hero6)+hero+new+hero4+hero5+hero6}")
-            
+            c(print(cess(hero+new+hero4+hero5+hero6)+hero+new+hero4+hero5+hero6))
+            c(speak(f"You have to give{cess(hero+new+hero4+hero5+hero6)+hero+new+hero4+hero5+hero6}"))
+c(speak("Enter your gross income (including other income if you have)"))            
 p=int(input("Enter your gross income(including other income if you have):"))
+
 
 q=input("Do you have any deductions? write'y' for yes and 'n' for no:")
 #These are for all the deductions 
